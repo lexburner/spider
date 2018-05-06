01d2be9489419830e9e0554064237f4f6b9d6575
xujingfeng <250577914@qq.com>
2018-05-07 01:23:17 +0800

    emmmm

--- src/main/java/moe/cnkirito/spider/javlibrary/SpiderApplication.java
+++ src/main/java/moe/cnkirito/spider/javlibrary/SpiderApplication.java
@@ -4,16 +4,27 @@ import com.geccocrawler.gecco.GeccoEngine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 //@SpringBootApplication
 public class SpiderApplication {
 
     public static void main(String[] args) {
 //        SpringApplication.run(SpiderApplication.class, args);
+        String proxyHost = "192.168.31.77";
+        String proxyPort = "1080";
+
+        System.setProperty("http.proxyHost", proxyHost);
+        System.setProperty("http.proxyPort", proxyPort);
+
+// 对https也开启代理
+        System.setProperty("https.proxyHost", proxyHost);
+        System.setProperty("https.proxyPort", proxyPort);
         GeccoEngine.create()
                 //工程的包路径
                 .classpath("moe.cnkirito.spider.javlibrary")
                 //开始抓取的页面地址
-                .start("http://www.javlibrary.com/cn/genres.php")
+                .start("http://www.javlibrary.com/cn/vl_genre.php?g=i4")
                 //开启几个爬虫线程
                 .thread(1)
                 //        //单个爬虫每次抓取完一个请求后的间隔时间
--- src/test/java/moe/cnkirito/spider/SpiderApplicationTests.java
+++ src/test/java/moe/cnkirito/spider/SpiderApplicationTests.java
@@ -1,16 +1,30 @@
 package moe.cnkirito.spider;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class SpiderApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	public static void main(String[] args) throws IOException {
+		String proxyHost = "192.168.31.77";
+		String proxyPort = "1080";
+
+		System.setProperty("http.proxyHost", proxyHost);
+		System.setProperty("http.proxyPort", proxyPort);
+
+// 对https也开启代理
+		System.setProperty("https.proxyHost", proxyHost);
+		System.setProperty("https.proxyPort", proxyPort);
+
 	}
 
 }
